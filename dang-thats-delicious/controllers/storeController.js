90715c63a514162418b2434e0ed8b2cea04e6e4e
Tariq Khawaja <tariqkhawaja26@live.com>
2021-12-14 23:12:49 +0000

    saving lat and lng for each store

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -12,7 +12,7 @@ exports.addStore = (req, res) => {
 
 exports.createStore = async (req, res) => {
     const store = await (new Store(req.body)).save();
-    req.flash('success' `Successfully Created ${store.name}. Care to leave a review?`);
+    req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
     res.redirect(`/store/${store.slug}`);
 };
 
