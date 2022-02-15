f199eadec5a6f518fbc7eb42dc7d262242e3515d
Tariq Khawaja <tariqkhawaja26@live.com>
2022-02-15 08:16:25 +0000

    19 - Using Pre-Save hooks to make Unique Slugs

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -1,5 +1,21 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+const { resize } = require('jimp');
+
+const multerOptions = {
+    storage: multer.memoryStorage(),
+    fileFilter(req, file, next) {
+        const isPhoto = file.mimetype.startsWith('image/');
+        if (isPhoto) {
+            next(null, true);
+        } else {
+            next({ message: 'That filetype isn\'t allowed!' }, false);
+        }
+    }
+}
 
 
 exports.homePage = (req, res) => {
@@ -9,6 +25,24 @@ exports.homePage = (req, res) => {
 exports.addStore = (req, res) => {
     res.render('editStore', { title: 'Add Store' });
 };
+
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async (req, res, next) => {
+    // check if there is no new file to resize
+    if (!req.file) {
+        next(); // skip to the next middleware
+        return;
+    }
+    const extension = req.file.mimetype.split('/')[1];
+    req.body.photo = `${uuid.v4()}.${extension}`;
+    // now we resize
+    const photo = await jimp.read(req.file.buffer);
+    await photo.resize(800, jimp.AUTO);
+    await photo.write(`./public/uploads/${req.body.photo}`);
+    // once we have written the photo to our filesystem, keep going!
+    next();
+}
 
 exports.createStore = async (req, res) => {
     const store = await (new Store(req.body)).save();
@@ -42,4 +76,11 @@ exports.updateStore = async (req, res) => {
     res.redirect(`/stores/${store._id}/edit`);
     //Redirect them the store and tell them it worked
 
-}
+};
+
+exports.getStoreBySlug = async (req, res, next) => {
+    const store = await Store.findOne({ slug: req.params.slug });
+    if (!store) return next();
+    res.render('store', { store, title: store.name });
+
+};
