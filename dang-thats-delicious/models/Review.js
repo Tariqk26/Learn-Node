6b3712783a0afeeb455c426c0e2e6a7f5e71027e
Tariq Khawaja <tariqkhawaja26@live.com>
2022-08-11 20:02:59 +0100

    37 + 38 Adding Reviews to Data Model and Relationship Population Displaying Reviews

--- dang-thats-delicious/models/Review.js
+++ dang-thats-delicious/models/Review.js
@@ -4,29 +4,35 @@ mongoose.Promise = global.Promise;
 const reviewSchema = new mongoose.Schema({
     created: {
         type: Date,
-        default: Date.now 
+        default: Date.now
     },
     author: {
         type: mongoose.Schema.ObjectId,
-        ref: 'User', 
+        ref: 'User',
         required: 'You must supply an author!'
-    }, 
+    },
     store: {
         type: mongoose.Schema.ObjectId,
-        ref: 'Store', 
+        ref: 'Store',
         required: 'You must supply a store!'
-    }, 
-    text: { 
-        type: { 
-            type: String,
-            required: 'Your review must have text!'
-        }
     },
-    rating: { 
+    text: {
+        type: String,
+        required: 'Your review must have text!'
+    },
+    rating: {
         type: Number,
-        min: 1, 
-        max: 5 
+        min: 1,
+        max: 5
     }
 });
+
+function autopopulate(next) {
+    this.populate('author');
+    next();
+}
+
+reviewSchema.pre('find', autopopulate);
+reviewSchema.pre('findOne', autopopulate);
 
 module.exports = mongoose.model('Review', reviewSchema);
